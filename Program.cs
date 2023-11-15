7680e811c23fe859b95f1714bee9a0519cc71184
IKVolod <i.k.volod@gmail.com>
2023-11-15 22:53:45 +0200

    k1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.IO;
+using System.Runtime.CompilerServices;
+using OOP_Lab_3;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Task1.Start();
+            Task2.Start();
+        }
+    }
+}
---
+++ Task2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace OOP_Lab_3
+{
+    internal class Task2
+    {
+        public static void Start()
+        {
+            Bitmap a;
+        }
+    }
+}
